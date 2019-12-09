de64876c08f98a094f4db5cc6ab0ef2dad91d6b4
PauMB <paumb85@gmail.com>
2019-12-09 13:43:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logoMono from './logo-mono.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logoMono} className="App-logo" alt="logo" />
+        <p>
+          We are working ;)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
